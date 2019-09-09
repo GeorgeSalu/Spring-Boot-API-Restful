f2aa7a41e07f6adaa3d45fa83e1a94a00b14bca3
George Salu <george.salu10@gmail.com>
2019-09-09 15:32:16 -0300

    simplificando o json de erro

---
+++ src/main/java/br/com/alura/forum/config/validacao/ErroDeFormularioDto.java
@@ -0,0 +1,32 @@
+package br.com.alura.forum.config.validacao;
+
+/**
+ * @author george on 09/09/2019
+ * @project forum
+ */
+public class ErroDeFormularioDto {
+
+    private String campo;
+    private String erro;
+
+    public ErroDeFormularioDto(String campo, String erro) {
+        this.campo = campo;
+        this.erro = erro;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+
+    public void setErro(String erro) {
+        this.erro = erro;
+    }
+}
