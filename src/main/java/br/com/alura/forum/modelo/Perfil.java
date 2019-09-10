23d4975527625c932d7b9b78810770594c2bc27b
George Salu <george.salu10@gmail.com>
2019-09-10 18:03:10 -0300

    restringindo o acesso aos endpoints privados

--- src/main/java/br/com/alura/forum/modelo/Perfil.java
+++ src/main/java/br/com/alura/forum/modelo/Perfil.java
@@ -1,7 +1,16 @@
 package br.com.alura.forum.modelo;
 
-public class Perfil {
+import org.springframework.security.core.GrantedAuthority;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Perfil implements GrantedAuthority {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 
@@ -22,4 +31,8 @@ public class Perfil {
 		return nome;
 	}
 
+	@Override
+	public String getAuthority() {
+		return nome;
+	}
 }
